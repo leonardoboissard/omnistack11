2bb18796b2b4668f9702cda4d7afe26daa2430b5
Leonardo Boissard <leonardoboissard@gmail.com>
2020-03-25 15:51:50 -0300

    created migrations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const crypto = require('crypto')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
     res.json({
         status: 'ok'
     })
+})
+
+routes.post('/ongs', (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body
+    const id = crypto.randomBytes(4).toString('HEX')
+
+    console.log(name)
+    return res.json()
 })
 
 module.exports = routes
